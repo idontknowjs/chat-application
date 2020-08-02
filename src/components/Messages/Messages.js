3f36360dd15dd2c66a88beb2b536c5ef27417ee5
Arijit Kundu <kunduarijit55@gmail.com>
2020-08-02 18:56:06 +0530

    Loader included

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,30 +1,41 @@
 import React from "react";
-
 import ScrollToBottom from "react-scroll-to-bottom";
-
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Message from "./Message/Message";
-
+import withStyles from "@material-ui/core/styles/withStyles";
 import "./Messages.css";
 
-const Messages = ({ messages, name }) => {
-  return (
+const styles = () => ({
+  loader: {
+    color: "#2979ff",
+    margin: "auto"
+  }
+});
+const Messages = ({ messages, name, classes }) => {
+  return messages.length > 0 ? (
     <ScrollToBottom className="messages">
-      {/* TODO: */}
-      {/* onClick=
-      {this.clickHandle} */}
-      {/* {
-        (onclick = () => {
-          console.log(document);
-          document.querySelector(".emoji-mart").style.display = "none";
-        })
-      } */}
       {messages.map((message, i) => (
         <div key={i}>
           <Message message={message} name={name} />
         </div>
       ))}
     </ScrollToBottom>
+  ) : (
+    <CircularProgress className={classes.loader} thickness="2.5" size="100px" />
   );
 };
 
-export default Messages;
+export default withStyles(styles)(Messages);
+
+// {
+//   onClick =
+//     { this.clickHandle };
+// }
+// {
+//   {
+//        (onclick = () => {
+//           console.log(document);
+//           document.querySelector(".emoji-mart").style.display = "none";
+//         })
+//       }
+// }
